27b26da53fdb2aa8e74d7175034d97d991374132
RickelviC <Cruzrickelvi@gmail.com>
2026-04-21 15:28:46 -0400

    added scanner bufferedwriter and localtime

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("logs.txt"));
+
+            LocalDateTime rightNow = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+
+
+        }catch (Exception ex){
+            System.err.println("something went wrong");
+        }
+    }
 }
