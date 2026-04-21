483cbded629b515d5f51ed06679cb606b6f930d5
RickelviC <Cruzrickelvi@gmail.com>
2026-04-21 15:38:23 -0400

    made the user input save in the log file

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -10,11 +10,16 @@ public class Main {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("logs.txt"));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("logs.txt", true));
 
             LocalDateTime rightNow = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
+            System.out.println("what would you like to search (press x to quit): ");
+            String newLine = scanner.nextLine();
+            bufferedWriter.write(newLine);
+
+            bufferedWriter.close();
 
 
         }catch (Exception ex){
